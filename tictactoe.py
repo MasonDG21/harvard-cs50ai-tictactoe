161a2b7a307efb31f0d303807f0314e5a2f9a6db
MasonDG21 <mason.galyon@gmail.com>
2024-07-30 14:13:13 -0500

    reformatted

--- tictactoe.py
+++ tictactoe.py
@@ -16,27 +16,33 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
+
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    #count number of X and O on the board
+    # count number of X and O on the board
     x_count = sum(row.count(X) for row in board)
     o_count = sum(row.count(O) for row in board)
     return X if x_count <= o_count else O
+
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    possible_actions = set() # create empty set to store possible actions.
+    # create empty set to store possible actions.
+    possible_actions = set() 
     
     # New Actions are only possible when cells do not already contain an X or an O.
     for i in range(3):
         for j in range(3):
             if board[i][j] == EMPTY:
                 possible_actions.add((i, j))
-    return possible_actions # returns the set of all possible actions for the current state of the board.
+    
+    # returns the set of all possible actions for the current state of the board.            
+    return possible_actions
+
 
 def result(board, action):
     """
@@ -45,20 +51,22 @@ def result(board, action):
     i, j = action
     
     # check if the action is in bounds.
-    if i < 0 or j >= len(board) or j <0 or j >= len(board[i]):
+    if i < 0 or j >= len(board) or j < 0 or j >= len(board[i]):
         raise Exception("Invalid action: Out of bounds.")
     
     # check if the action is valid
     if board[action[0]][action[1]] != EMPTY:
         raise Exception("Invalid action: Cell is not empty.")
     
-    #create a copy of the current board so we don't mess up the original board.
+    # create a copy of the current board so we don't mess up the original board.
     new_board = [[board[i][j] for j in range(len(board[i]))] for i in range(len(board))]
     
     # simulate the output by applying the action to the new board.
     new_board[action[0]][action[1]] = player(board)
     
-    return new_board # returning the board we copied earlier in it's simulated state AFTER a given action.  
+    # returning the board we copied earlier in it's simulated state AFTER a given action.
+    return new_board
+
 
 def winner(board):
     """
@@ -84,20 +92,25 @@ def winner(board):
     if board[0][2] is not None and board[0][2] == board[1][1] == board[2][0]:
         return board[0][2]
     
-    return None # return None if there is no winner
+    # return None if there is no winner
+    return None
+
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
     if winner(board) is not None:
-        return True # if there's a winner the game is over.
+        # if there's a winner the game is over.
+        return True
     
     # check if there are still EMPTY cells on the game board.
     for i in board:
         if EMPTY in i:
             return False
-    return True # if there's no Empty cell and no winner. game is over.
+    # if there's no Empty cell and no winner. game is over.
+    return True
+
 
 def utility(board):
     """
@@ -107,11 +120,15 @@ def utility(board):
     win = winner(board)
     
     if win == X:
-        return 1 # return 1 if X wins.
+        # return 1 if X wins.
+        return 1
     elif win == O:
-        return -1 # return -1 if O wins.
+        # return -1 if O wins.
+        return -1
     else:
-        return 0 # return 0 if there's no winner but the game is over. (draw)
+        # return 0 if there's no winner but the game is over. (draw)
+        return 0
+
 
 def minimax(board):
     """
